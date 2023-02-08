d74d3d66df3b7a9891fb31ae4acb70603baf0ee6
digitaleAffen <digitaleaffen@gmail.com>
2023-02-08 20:15:44 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      Hello
+      <footer>
+        <img src={logo} className="App-logo" alt="logo" width="40" />
+      </footer>
+    </div>
+  );
+}
